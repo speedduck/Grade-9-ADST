893399cc9c9330d2879d706ce0382d65e0478714
Sean Hulka <sean.hulka@gmail.com>
2019-01-21 14:11:59 -0800

    Added date stamp to messages.
    Signed-off-by: Sean Hulka <sean.hulka@gmail.com>

--- ws/server.js
+++ ws/server.js
@@ -4,9 +4,9 @@ var WebSocketServer = require('ws').Server
     , wss = new WebSocketServer({port: 8010});
     wss.on('connection', function(ws) {
         ws.on('message', function(message) {
-            console.log('Received from client: %s', message);
+            console.log('Received from client: %s', new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''), message);
             wss.clients.forEach(function each(client) {
-     		client.send(message);
+     		client.send(new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''), message);
 	    });
 	});
     });
